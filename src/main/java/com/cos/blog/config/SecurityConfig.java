4b0ae419f9181951559b84062133a7e4d84f4516
dmstlf292 <silviapark292@gmail.com>
2021-03-22 01:48:25 +0900

    Finish Security Login Service

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.cos.blog.config.auth.PrincipalDetailService;
 
 
 	
@@ -18,12 +22,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity// 시큐리티에 필터가 등록이 된다. 
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private PrincipalDetailService principalDetailService;
+	
+	
+	
+	
 
 	@Bean // IoC 가 된다@@
 	public BCryptPasswordEncoder encodePWD() {
 		//String encPassword =  new BCryptPasswordEncoder().encode("1234");
 		return new BCryptPasswordEncoder();
 	}
+	
+	//시큐리티가 대신 로그인해주는데 password를 가로채기해서 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야 
+	//같은 해쉬로 암호화 해서 디비에 있는 해쉬랑 비교할 수 있다. 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -36,7 +61,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.authenticated()
 			.and()
 				.formLogin()
-				.loginPage("/auth/loginForm");
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc")
+				.defaultSuccessUrl("/");//스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준
 			
 	}
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,7 +13,8 @@ import com.cos.blog.model.User;
 //@Repository 가 생략이 가능하다 
 public interface UserRepository extends JpaRepository<User, Integer>{
 	
-	
+	//select * from user where username=1?;
+	Optional<User> findByUsername(String username);
 	
 	
 }
